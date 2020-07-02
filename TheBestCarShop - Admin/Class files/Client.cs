99187f023ebf109df6bc7b8a793d2c3aa0faaed9
rot4ry <wojtekw233@gmail.com>
2020-07-02 18:06:47 +0200

    Added first files

---
+++ TheBestCarShop - Admin/Class files/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestCarShop
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string CompanyName { get; set; } //not a required field
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Postcode { get; set; }
+        public string BuildingNumber { get; set; }
+
+        //login data
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int IsAdmin { get; set; }
+
+    }
+}
---
+++ TheBestCarShop - Admin/Class files/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBestCarShop
+{
+    public class Product
+    {
+        public int ProductID { get; private set; }
+        //this part fits in:
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public int CarFirstProdYear { get; set; }
+        public int CarLastProdYear { get; set; }
+
+        //and costs:
+        public decimal Price { get; set; }
+
+        //product details
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public string PartCode { get; set; }
+        public bool IsAvailable { get; set; }
+        public int Quantity { get; set; }
+    }
+}
