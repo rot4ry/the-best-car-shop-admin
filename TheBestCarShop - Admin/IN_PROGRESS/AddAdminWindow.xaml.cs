07e0cc952aef6eb0872e116384fa67f968826ec3
rot4ry <wojtekw233@gmail.com>
2020-08-26 23:14:13 +0200

    Finished: Adding new Admin account.

--- TheBestCarShop - Admin/IN_PROGRESS/AddAdminWindow.xaml.cs
+++ TheBestCarShop - Admin/IN_PROGRESS/AddAdminWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
 using TheBestCarShop___Admin.Class_files.Basics;
 
 namespace TheBestCarShop___Admin.IN_PROGRESS
@@ -8,19 +11,8 @@ namespace TheBestCarShop___Admin.IN_PROGRESS
     /// </summary>
     public partial class AddAdminWindow : Window
     {
-        //FirstName, 
-        //SecondName,
-        //CompanyName,
-        //Email,
-        //Phone,
-        //Country,
-        //City,
-        //Street,
-        //Postcode,
-        //BuildingNumber,
-        //Username,
-        //Password,
         private Client _ADMIN;
+        private DatabaseHandler databaseHandler = new DatabaseHandler();
 
         public AddAdminWindow(Client admin)
         {
@@ -34,18 +26,85 @@ namespace TheBestCarShop___Admin.IN_PROGRESS
         }
         private void generateUsernameButton_Click(object sender, RoutedEventArgs e)
         {
-            usernameTB.Text = "testLoginu";
-        }
-        private void generatePasswordButton_Click(object sender, RoutedEventArgs e)
-        {
-            passwordTB.Text = "testHasla";
+            //This code is basically copied from TheBestCarShop project
+            //in order to maintain integrity of names kept in the database
+            string name = firstNameTB.Text;
+            string secondName = secondNameTB.Text;
+            string variableToken = new Random().Next(10, 1000).ToString();
+
+            try
+            {
+                name = name.Remove(new Random().Next(3, name.Length));
+                secondName = secondName.Remove(new Random().Next(3, secondName.Length));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            string generatedUsername = new StringBuilder()
+                .Append(name)
+                .Append(secondName)
+                .Append(variableToken)
+                .ToString();
+
+            usernameTB.Text = generatedUsername;
         }
 
+        private void generatePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            int lengthOfPassword = new Random().Next(8, 10);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 1; i <= lengthOfPassword; i++)
+            {
+                GC.Collect();
+                Random random = new Random();
+                char c = (char)random.Next(33, 126);
+                random.Next(1, 1000);
+                GC.Collect();
+                
+                sb.Append(c);
+            }
+            string randomizedPassword = sb.ToString();
+
+            passwordTB.Text = randomizedPassword;
+        }
 
         private void addAdminButton_Click(object sender, RoutedEventArgs e)
         {
-            //insert into the DB
-            //open a windof for confirmation -> username and password
+            addAdminButton.IsEnabled = false;
+            //open a window for confirmation -> username and password
+            
+            int result = databaseHandler.AddAdmin(new Client
+                {
+                    FirstName       = @firstNameTB.Text,
+                    SecondName      = @secondNameTB.Text,
+                    CompanyName     = @companyNameTB.Text,
+                    Email           = @emailTB.Text,
+                    PhoneNumber     = @phoneTB.Text,
+                    Country         = @countryTB.Text,
+                    City            = @cityTB.Text,
+                    Street          = @streetTB.Text,
+                    Postcode        = @postcodeTB.Text,
+                    BuildingNumber  = @buildingTB.Text,
+                    Username        = @usernameTB.Text,
+                    Password        = @passwordTB.Text,
+                    IsAdmin         = 1
+                }
+            ); 
+
+            if(result == 1)
+            {
+                MessageBox.Show("A new account has been created.", "Success!", MessageBoxButton.OK);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Something went wrong, please try again.", "Failure!", MessageBoxButton.OK);
+                this.Close();
+            }
+
         }
     }
 }
